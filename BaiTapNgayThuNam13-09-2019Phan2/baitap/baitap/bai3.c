3a3fa84b0fde53790d85d03c4c1d5f217093ba0c
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-09-18 15:15:46 +0700

    update

---
+++ BaiTapNgayThuNam13-09-2019Phan2/baitap/baitap/bai3.c
@@ -0,0 +1,18 @@
+//
+//  bai3.c
+//  baitap
+//
+//  Created by Nguyen Duc Hoang on 9/18/19.
+//  Copyright © 2019 Nguyen Duc Hoang. All rights reserved.
+//
+
+#include <stdio.h>
+/*
+ Bài 3
+ Các phát biểu sau là đúng hay sai:
+ C is case sensitive. => False
+ The first character of the identifier can be a number. => False
+ Variable's name can contain space character. => False
+ Variable's name can be Uppercase => True
+ 
+ */
