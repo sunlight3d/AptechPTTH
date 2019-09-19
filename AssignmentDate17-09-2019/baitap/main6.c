ba3a47cc26d452df93b9fe50387ec8fe232d351c
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-09-19 21:10:58 +0700

    update

--- AssignmentDate17-09-2019/baitap/main6.c
+++ AssignmentDate17-09-2019/baitap/main6.c
@@ -11,15 +11,22 @@
  else
  goto label1:
  Test: printf("All done...”);
+ Thay đoạn chương trình trên thành do-while.
  */
 int main(int argc, char *argv[]) {
     int num ;
-    label1: printf("\nEnter a number (1) :");
-    scanf("%d",&num);
-    if(num == 1)
-        goto Test;
-    else
-        goto label1;
-    Test: printf("All done...");
+//    label1: printf("\nEnter a number (1) :");
+//    scanf("%d",&num);
+//    if(num == 1)
+//        goto Test;
+//    else
+//        goto label1;
+//    Test: printf("All done...");
+    //Thay đoạn chương trình trên thành do-while.
+    do {
+        printf("\nEnter a number (1) :");
+        scanf("%d",&num);
+    }while (num != 16);
+    printf("All done...");
     return 0;
 }
