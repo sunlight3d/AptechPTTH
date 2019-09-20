410ed69468279ab9713bd96248b53e51a11164a5
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-09-20 09:13:39 +0700

    update

--- LabSessionDate20-09-2019/main2.c
+++ LabSessionDate20-09-2019/main2.c
@@ -15,16 +15,27 @@
 1 2 3 4 5 6 7 8 9
 G?i ý: Tham kh?o bài "tam giác d?u *" dã h?c ? bài lý thuy?t for, while
 */
-int main(int argc, char *argv[]) {
+int main2(int argc, char *argv[]) {
 	//Dau vao, ten va tuoi
 	int rows, columns;
 	int N;
 	printf("Nhap N = "); scanf("%d", &N);
-	for(rows = 0; rows < N+1; rows++){
-		for(columns = 0;columns < rows; columns++) {
+//	for(rows = 0; rows < N+1; rows++){
+//		for(columns = 0;columns < rows; columns++) {
+//			printf("%d", columns+1);
+//		}
+//		printf("\n");
+//	}
+	//Chuyen sang while
+	rows = 0;
+	while(rows < N+1) {
+		columns = 0;
+		while(columns < rows) {
 			printf("%d", columns+1);
+			columns++;
 		}
 		printf("\n");
+		rows++;
 	}
 	return 0;
 }
---
+++ LabSessionDate20-09-2019/main3.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+3.Write a program to print a multiplication table for a given number.
+Bài này là hi?n ra "b?n c?u chuong"
+VD: B?n mu?n xem b?ng c?u chuong m?y: 5
+In ra: 5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+....
+5 x 10 = 50
+*/
+int main(int argc, char *argv[]) {
+	//Input: N
+	int N, rows, columns;
+	printf("Enter N ="); scanf("%d", &N);
+	for(rows = 0; rows < 10; rows++){
+		int multiply = N * (rows + 1);
+		printf("%d x %d = %d\n",N, rows + 1, multiply);
+	}
+	
+	return 0;
+}
