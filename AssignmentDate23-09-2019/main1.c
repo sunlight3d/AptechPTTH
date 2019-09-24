984504b1a35e7be14199410fef6e5fb7778c7b6a
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-09-24 09:30:31 +0700

    update

---
+++ AssignmentDate23-09-2019/main1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/*
+1.Write a program to arrange the following names in alphabetical order.
+George
+Albert
+Tina
+Xavier
+Roger
+Tim
+William
+*/
+
+int main(int argc, char *argv[]) {
+	//Initialize array
+	char name[7][10];
+	int i, j;
+	strcpy(name[0], "George");
+	strcpy(name[1], "Albert");
+	strcpy(name[2], "Tina");
+	strcpy(name[3], "Xavier");
+	strcpy(name[4], "Roger");
+	strcpy(name[5], "Tim");
+	strcpy(name[6], "William");
+	for(i = 0; i < 6; i++){
+		for(j = i + 1; j < 7; j++){
+			if(strcmp(name[i], name[j]) > 0) {
+				char strtemp[50];
+				//swap name[i], name[j]
+				strcpy(strtemp, name[i]);
+				strcpy(name[i], name[j]);
+				strcpy(name[j], strtemp);
+			}
+		}
+	}
+	for(i = 0; i < 7; i++) {
+		printf("%s\n", name[i]);
+	}
+	return 0;
+}
