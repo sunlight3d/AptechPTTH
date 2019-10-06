7e0f9c643efc83a66991c2e07995118c5eef1f7e
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-10-06 12:40:18 +0700

    update

--- BaiTapTongHop/main.c
+++ BaiTapTongHop/main.c
@@ -77,15 +77,90 @@ struct Product {
 	float price;
 	char category[];
 }
+struct CategoryAnalyze {
+	int numberOfProducts;
+	char category[];
+}
 
 void input(struct Product *product) {
+	int N, i;
+	printf("Number of products : \n");scanf("%d", &N);
+	for(i = 0; i < N; i++){
+		product+i = (Product *)malloc(sizeof(Product *));
+		printf("Enter product's name : \n");scanf("%s", &(*(product+i).name));
+		printf("Enter product's year : \n");scanf("%d", &(*(product+i).year));
+		printf("Enter product's price : \n");scanf("%f", &(*(product+i).price));
+		while(*(product+i).price <= 0 || *(product+i).price >= 1000) {
+			printf("Enter product's price : \n");scanf("%f", &(*(product+i).price));
+		}
+		printf("Enter product's category : \n");scanf("%s", &(*(product+i).category));
 
+	}
 }
 void sort(struct Product *product) {
+	int i, j;
+	if(!product) {
+		printf("Please input array first !\n");
+		break;
+	}
+	for(i = 0; i < N-1; i++){
+		for(j = i+1; j < N; j++){
+			if(strcmp(*(product+i).name, *(product+j).name) > 0) {
+				struct Product temp = *(product+i);
+				*(product+i) = *(product+j);
+				*(product+j) = temp;				
+			}
+		}	
+	}	
+	//Hien thi ket qua
+	for(i = 0; i < N; i++){
+		printf("Product %d details: \n", i+1);
+		printf("Name : %s\n", *(product+i).name);
+		printf("Year : %d\n", *(product+i).year);
+		printf("Price : %d\n", *(product+i).price);
+		printf("Category : %s\n", *(product+i).category);
+	}
+}
+int checkCategoryAnalyzeExist(struct CategoryAnalyze *categoryAnalyze, char category[]){
+	struct CategoryAnalyze *ptr;
+	//Dung con tro ptr duyet tu phan tu dau den phan tu cuoi cua mang
+	for (ptr = categoryAnalyze; (*ptr).name != NULL ; ptr++){
+		if(strcmp(*ptr.category, category) == 0){
+			return 1;
+		}
+	}	
+	return 0;
+}
+void insertCategoryAnalyze(struct CategoryAnalyze *categoryAnalyze, char category[]){
+	struct CategoryAnalyze *ptr;
+	if(checkCategoryAnalyzeExist((*ptr).category) == 1) {
+		//Neu tim thay category trong mang categoryAnalyze
+		//Dung con tro ptr duyet tu phan tu dau den phan tu cuoi cua mang
+		for (ptr = categoryAnalyze; (*ptr).name != NULL ; ptr++){
+			if(strcmp((*ptr).category, category) == 0){
+				categoryAnalyze.numberOfProducts++;
+			}
+		}		
+	} else {
+		//Neu KO tim thay category trong mang categoryAnalyze
+		//Them 1 phan tu vao cuoi categoryAnalyze
+		CategoryAnalyze *endPoint;
+		endPoint = (CategoryAnalyze *)malloc(sizeof(CategoryAnalyze *));
+		strcpy((*endPoint).name, "");
+		strcpy((*endPoint).category, category);				
+		struct CategoryAnalyze *ptr;
+		for (ptr = categoryAnalyze; (*ptr).name != NULL ; ptr++){}
+		ptr = endPoint;	
+	}
+	
+	
 	
 }
+
 void analyze(struct Product *product) {
-	
+	for(i = 0; i < N; i++) {
+		
+	}	
 }
 void find() {
 	
@@ -104,39 +179,31 @@ int main() {
 	
 	char choice;
 	printf("Enter a choice : \n"); choice = getchar();
+	getchar();//Lenh nay loai tru ky tu enter cua dong truoc
 	struct Product *product;	
 	switch(choice) {
 		case '1':
-			input();
-			goto LABEL2;
+			input(); goto LABEL2;
 			break;
 		case '2':
-			sort();
-			goto LABEL2;
-			break;
+			sort(); goto LABEL2; break;
 		case '3':
-			analyze();
-			goto LABEL2;
+			analyze(); goto LABEL2;
 			break;
 		case '4':
-			find();
-			goto LABEL2;
+			find(); goto LABEL2;
 			break;
 		case '5':
-			save();
-			goto LABEL2;
+			save();goto LABEL2;
 			break;
 		case '6':
-			open();
-			goto LABEL2;
+			open();goto LABEL2;
 			break;
 		case '7':
 			return 0;//ket thuc chuong trinh
-			goto LABEL2;
-			break;
+			goto LABEL2;break;
 		default:
-			printf("Please choose 1->7\n");
-			goto LABEL1;
+			printf("Please choose 1->7\n"); goto LABEL1;
 			break;
 	}
 	LABEL2: 
@@ -144,6 +211,22 @@ int main() {
 	printf("- Yes, I do. (press ‘y’, ‘Y’)\n");
 	printf("- No, I don’t. (press ‘n’, ‘N’)\n");
 	printf("- Clear the screen ! (press ‘c’, ‘C’)\n");
+	switch(choice) {
+		case 'y':
+		case 'Y':		
+			goto LABEL1;
+			break;
+		case 'n':
+		case 'N':			
+			break;
+		case 'c':
+		case 'C':
+			//clear screen
+			system("@cls||clear");
+			break;		
+		default:			
+			break;
+	}	
 	return 0;
 }
 
