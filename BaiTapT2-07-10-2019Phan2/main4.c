cad23ba3131e8bb28a3518d3171f780c3d7405fc
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-10-09 19:03:57 +0700

    update

--- BaiTapT2-07-10-2019Phan2/main4.c
+++ BaiTapT2-07-10-2019Phan2/main4.c
@@ -6,6 +6,7 @@ struct Product {
     char name[100];
     unsigned int manufacture_year;
     char manufacture[200];
+    float price;
 };
 int N;//bien toan cuc, co tac dung ben trong main.c
 struct Product *input(){
@@ -17,6 +18,7 @@ struct Product *input(){
         printf("Enter product's name : \n");scanf("%s", (*(product+i)).name);
         printf("Enter product's year : \n");scanf("%d", &((*(product+i)).manufacture_year));
         printf("Enter product's manufacture : \n");scanf("%s", (*(product+i)).manufacture);
+        printf("Enter product's price : \n");scanf("%f", &(*(product+i)).price);
     }
     return product;
 }
@@ -43,12 +45,39 @@ void display(struct Product *product) {
         printf("Product's manufacture : %s\n", (*(product+i)).manufacture);
     }
 }
-
+void find(struct Product *product) {
+    printf("Find some products \n");
+    struct Product *productResults = (struct Product *)malloc(sizeof(struct Product));
+    struct Product *ptr;
+    int i = 0;
+    float maxPrice, minPrice;
+    char manufacture[200];
+    printf("Product's manufacture: "); scanf("%s", manufacture);
+    printf("Enter min price: "); scanf("%f", &minPrice);
+    printf("Enter max price: "); scanf("%f", &maxPrice);
+    ptr = productResults;
+    int j = 0; //Tong so ket qua
+    for(i = 0; i < N; i++){
+        if(strcmp((*(product+i)).manufacture, manufacture) == 0 &&
+           (*(product+i)).price >= minPrice && (*(product+i)).price <= maxPrice ){
+            ptr = (struct Product *)malloc(sizeof(struct Product));
+            ptr++;
+            j++;
+        }
+    }
+    printf("Search result : \n");
+    for(i = 0; i < j; i++){
+        printf("Product's name : %s\n", (*(product+i)).name);
+        printf("Product's year : %d\n", (*(product+i)).manufacture_year);
+        printf("Product's manufacture : %s\n", (*(product+i)).manufacture);
+        printf("Product's price : %f\n", (*(product+i)).price);
+    }
+}
 int main(int argc, char * argv[]) {
     //Tao menu
-    printf("+--------------------------------------------------+\n");
-    printf("+ 1.Input some Product  2.Sort  3.Display  4.Exit. +\n");
-    printf("+--------------------------------------------------+\n");
+    printf("+----------------------------------------------------------+\n");
+    printf("+ 1.Input some Product  2.Sort  3.Display  4.Find. 5.Exit. +\n");
+    printf("+----------------------------------------------------------+\n");
     struct Product *product = NULL;
     char choice;
     LABEL1: printf("Enter your choice : "); choice = getchar();
@@ -69,6 +98,10 @@ int main(int argc, char * argv[]) {
             goto LABEL2;
             break;
         case '4':
+            find(product);
+            goto LABEL2;
+        break;
+        case '5':
             return 0;
             break;
         default:
