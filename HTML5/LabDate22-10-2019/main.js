6cb586d59e549549ede29fff7fd552d50ee281c1
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-10-22 15:03:35 +0700

    update

---
+++ HTML5/LabDate22-10-2019/main.js
@@ -0,0 +1,5 @@
+function login(){  
+    var name = document.getElementById("name").value
+    var password = document.getElementById("password").value
+    alert("name = "+name+", password: "+password)
+}
